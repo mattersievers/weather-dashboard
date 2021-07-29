d5bd34ef264b35f3ac8aba360c1750f8fafdeeec
Matthew Sievers <mattersievers@gmail.com>
2021-07-29 02:56:01 -0500

    added button handler. found api that inputs city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,39 @@
-/*
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + {lat} + "&lon=" + {lon} + "&exclude=" + {part} + "&appid=" +{API key})
-.then(function(response){
-    response.json()
-})
-.then(function(data){
-});
+var formEl = document.querySelector("#city-search-form");
+var inputEl = document.querySelector("#input-city-name")
 
+//Submit button is clicked
+var submitButtonHandler = function(event) {
+  event.preventDefault();
+  var currentCity = inputEl.value.trim();
+  if(currentCity) {
+    inputEl.value = "";
+    getWeather(currentCity);
+  } else {
+      alert("Please enter a city.")
+  }  
+};
+
+var getWeather = function(city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a5bd61827c7f6e73efae30569b5a38fa")
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+    });
+}
+
+getWeather("Austin");
+/*
+- Grab name of city from input.
+
+- get lat and long of city geocode coordinates of city
+- plug lat and long to openweather
+- retrieve information
+-plug information into current-weather
+-plug information into 5 day
+-save city search under the previous-city-holder
 */
 
+//event listener for Search button
+formEl.addEventListener("submit", submitButtonHandler);
